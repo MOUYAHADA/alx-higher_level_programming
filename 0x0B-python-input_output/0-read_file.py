4542bf2b6c5216cbf5a5667fc6e0a4ff79334a39
Mohamed Mouyahada <mouyahada@gmail.com>
2024-04-16 20:17:35 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for the read_file function """
+
+
+def read_file(filename=""):
+    """ Reads a text file and prints it to stdout """
+    with open(filename, encoding="UTF8") as f:
+        for line in f:
+            print(line, end="")
