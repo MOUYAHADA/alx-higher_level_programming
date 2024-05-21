49463b61820a308c4aecef0b84fd166384a54295
Mohamed Mouyahada <mouyahada@gmail.com>
2024-05-21 22:24:59 +0100

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results are ordered by score in descending order.
+SELECR `score`, `name` FROM `second_table`
+ORDER BY `score` DESC;
