3616d93abd16545fdef49d04290f9e07e9f5db03
Mohamed Mouyahada <mouyahada@gmail.com>
2024-07-30 23:28:39 +0100

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) { 
-		return console.error(err)
-	}
-	console.log(data)
-})
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log(data);
+});
